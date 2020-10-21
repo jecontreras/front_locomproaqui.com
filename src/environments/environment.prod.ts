81683f1b68c0e66a02e23905f28f9d1a065517b0
jecontreras <joseeduar147@gmail.com>
2020-10-21 01:08:29 -0500

    cambios new

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   url: "https://backlocomproaqui.herokuapp.com",
-  urlFront: "https://www.locomproaqui.com"
+  urlFront: "https://locomproaqui-e0ae4.web.app"
 };
