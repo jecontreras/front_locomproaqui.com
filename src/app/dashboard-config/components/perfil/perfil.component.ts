e7dc3357b066efb9ceb384c5019d04a6176857fb
jecontreras <joseeduardo147@gmail.com>
2021-07-29 20:47:53 -0500

    cambios pred

--- src/app/dashboard-config/components/perfil/perfil.component.ts
+++ src/app/dashboard-config/components/perfil/perfil.component.ts
@@ -127,7 +127,7 @@ export class PerfilComponent implements OnInit {
   }
 
   Actualizar(){
-    this.data = _.omit(this.data, ['usu_perfil', 'cabeza', 'nivel', 'empresa', 'createdAt', 'updatedAt', 'categoriaPerfil']);
+    this.data = _.omit(this.data, ['usu_perfil', 'cabeza', 'nivel', 'empresa', 'createdAt', 'updatedAt', 'categoriaPerfil']);this.data = _.omitBy(this.data, _.isNull);
     this._user.update(this.data).subscribe((res:any)=>{
       //console.log(res);
       this._tools.presentToast("Actualizado");
