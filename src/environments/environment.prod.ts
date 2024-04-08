a42bdb2238af04895f5ef12c3d382564815fe0b5
jecontreras <joseeduar147@gmail.com>
2024-04-08 09:46:45 -0500

    EDU ajuste api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //url: "https://backlocomproaqui.herokuapp.com",
-  url: 'https://apilokomprojs-46c477c2e9f6.herokuapp.com',
+  url: 'https://apilokomproaqui1-9219656b6da1.herokuapp.com',
   urlEnvios: "https://apienviosrapidos-ce1b900c1c5b.herokuapp.com",
   urlFront: "https://lokomproaqui.com",
   keyEpayco: "fd1fee1f6f008ddf010440f8c092a2cb",
