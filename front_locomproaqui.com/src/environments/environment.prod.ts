bb4ab764404b550a595aabb8d357bc305f484070
jecontreras <joseeduar147@gmail.com>
2024-04-08 09:56:05 -0500

    EDU upd billetera!

--- front_locomproaqui.com/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  //url: "https://backlocomproaqui.herokuapp.com",
-  url : "https://apilokomproaqui1-9219656b6da1.herokuapp.com",
-  urlEnvios: "https://apienviosrapidos-ce1b900c1c5b.herokuapp.com",
-  urlFront: "https://lokomproaqui.com",
-  keyEpayco: "fd1fee1f6f008ddf010440f8c092a2cb",
-  estadoPruebaPagos: true
-};
